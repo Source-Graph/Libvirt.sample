18cb785ba043f2a65098f6c3da03bd1276fdf6c3
Dark Star☀🎵 <darkstar@unisocial.net>
2022-12-25 20:31:42 -0600

    works

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -3,7 +3,10 @@
 
 using namespace std;
 
-void lookupDomain(){
+void defaultConnection(){
+/*  Works in Ubuntu as regular user. Or in Fedora as root: `sudo ./learnLibvirt` But will NOT work in Fedora as a regular user.
+    Solution: https://bugzilla.redhat.com/show_bug.cgi?id=1714031
+*/
 
     int id {5};
     virConnectPtr c;
@@ -15,11 +18,36 @@ void lookupDomain(){
     name = virDomainGetName(d);
 
     printf("name of domain %d is %s\n", 2, name);
+    virConnectClose(c);
+}
+
+void systemConnection(){
+    /* Works for normal users! in Fedora
+    https://libvirt.org/api.html
+    https://libvirt.org/uri.html
+    https://libvirt.org/html/libvirt-libvirt-host.html#virConnectOpen
+    */
+
+    virConnectPtr c;
+    const char *uri;
+    uri = "qemu:///system";
+    c = virConnectOpen(uri);
+
+
+    int id {2};
+    virDomainPtr d;
+    const char *name;
+    d = virDomainLookupByID(c, id);
+    name = virDomainGetName(d);
+    printf("name of domain %d is %s\n", 2, name);
+
+    virConnectClose(c);
 }
 
 int main()
 {
-    lookupDomain();
+//    defaultConnection();
+    systemConnection();
     cout << "Hello world!" << endl;
     return 0;
 }
